e3f41c81a63b9295ce8a8ce654ef209fa84d2d2c
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-02-11 09:55:29 +0200

    add solution to task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const dayCost = 40;
+  let fullPrice = dayCost * days;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const shortTurm = 3;
+  const shortTurmDiscount = 20;
+
+  if (days >= longTerm) {
+    return (fullPrice -= longTermDiscount);
+  }
+
+  if (days >= shortTurm) {
+    return (fullPrice -= shortTurmDiscount);
+  }
+
+  return fullPrice;
 }
 
 module.exports = calculateRentalCost;
